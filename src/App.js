dac2554206843e3a10213abb060ad77e97d89a06
Abdoul_djabarou <babdouldjabar@yahoo.com>
2020-10-05 22:53:45 +0000

    Mise à jour de notre menu de navigation avec withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
+import {Switch,Route} from 'react-router-dom';
+
+const HatsPage= () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Switch>
+        <Route exact path='/' component={Homepage}/>
+        <Route  path='/hats' component={HatsPage}/>
+      </Switch>
+     
+      
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -11,32 +11,37 @@ class Directory extends React.Component {
                    title:'HATS',
                    imageUrl:'https://images.unsplash.com/photo-1568123326923-105aaedcee23?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
                    size:'large',
-                   id:1
+                   id:1,
+                   linkUrl:'hats'
                },
                {
                    title:'JACKETS',
                    imageUrl:'https://images.unsplash.com/photo-1551488831-00ddcb6c6bd3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
                    size:'large',
-                   id:2
+                   id:2,
+                   linkUrl:''
                },
 
                {
                 title:'SNEAKERS',
                 imageUrl:'https://images.unsplash.com/photo-1491553895911-0055eca6402d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80',
                 size:'large',
-                id:3
+                id:3,
+                linkUrl:''
                },
               {
                 title:'WOMENS',
                 imageUrl:'https://images.unsplash.com/photo-1470468969717-61d5d54fd036?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
                 size:'large',
-                id:4
+                id:4,
+                linkUrl:''
               },
               {
                 title:'MENS',
                 imageUrl:'https://images.unsplash.com/photo-1599577180675-470e88ebe8dd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
                 size:'large',
-                id:5
+                id:5,
+                linkUrl:''
                }
            ]
         }
@@ -46,8 +51,8 @@ class Directory extends React.Component {
         return(
             <div className='directory-menu'>
               {
-                this.state.sections.map(({title,imageUrl,id,large}) => (
-                    <MenuItem key={id} title={title} imageUrl={imageUrl} size={large} />
+                this.state.sections.map(({id, ...OtherSectionsProps}) => (
+                    <MenuItem key={id} {...OtherSectionsProps} />
                 ))
               }
              
