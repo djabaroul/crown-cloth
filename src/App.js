e40f24e3a37ada8a4e709fb3990c6ffa8dfa6afe
Abdoul_djabarou <babdouldjabar@yahoo.com>
2020-10-06 11:55:59 +0000

    Mise à jour de notre site avec la creation de la page shop ,collectpreview component, collectitem

--- src/App.js
+++ src/App.js
@@ -2,19 +2,16 @@ import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
 import {Switch,Route} from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component.jsx';
 
-const HatsPage= () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
+
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path='/' component={Homepage}/>
-        <Route  path='/hats' component={HatsPage}/>
+        <Route  path='/shop' component={ShopPage}/>
       </Switch>
      
       
