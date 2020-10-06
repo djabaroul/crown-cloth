1b9983371d0636387688179d7ca822f472b13f1e
Abdoul_djabarou <babdouldjabar@yahoo.com>
2020-10-06 16:01:16 +0000

    Ajout du composant header et les menus home,shop,contact

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Homepage from './pages/homepage/homepage.component';
 import {Switch,Route} from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 
 
 
 function App() {
   return (
     <div className="App">
+    <Header/>
       <Switch>
         <Route exact path='/' component={Homepage}/>
         <Route  path='/shop' component={ShopPage}/>
